86f250134b1ca03ff3d24f57826d193b9a63e6b0
Chen Xiaofeng <515318881@qq.com>
2018-04-14 21:49:28 +0800

    裁判系统数据测试正常

--- BSP/usart3.h
+++ BSP/usart3.h
@@ -2,4 +2,31 @@
 #define __USART3_H__
 #include "main.h"
 void USART3_Configuration(void);
+
+typedef enum
+{
+	ONLINE,
+	OFFLINE
+}JudgeState_e;
+
+typedef struct 
+{
+		uint8_t bulletType;
+		uint8_t bulletFreq;
+		float bulletSpeed;
+}extShootData_t;
+
+typedef struct 
+{
+    float chassisVolt;
+		float chassisCurrent;
+		float chassisPower;
+    float chassisPowerBuffer;
+		uint16_t shooterHeat0;
+    uint16_t shooterHeat1;
+}extPowerHeatData_t;
+
+extern extShootData_t extShootData;
+extern extPowerHeatData_t extPowerHeat;
+
 #endif
